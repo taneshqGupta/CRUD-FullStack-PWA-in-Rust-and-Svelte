b492d1054f5c93f6d95f7410c51464f5a154c5a6
taneshqGupta <f.taneshq@gmail.com>
2025-06-13 01:06:11 +0530

    refactored

--- frontend/src/lib/components/ThemeSwitcher.svelte
+++ frontend/src/lib/components/ThemeSwitcher.svelte
@@ -31,12 +31,7 @@
         role="button"
         class={`btn btn-ghost text-lg ${dropdownOpen ? 'bg-base-200' : ''}`}
     >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palette">
-            <rect x="5" y="3" width="14" height="18" rx="2"></rect>
-            <path d="M12 21V12"></path>
-            <path d="M5 9h14"></path>
-            <path d="M5 15h14"></path>
-        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palette-icon lucide-palette"><path d="M12 22a1 1 0 0 1 0-20 10 9 0 0 1 10 9 5 5 0 0 1-5 5h-2.25a1.75 1.75 0 0 0-1.4 2.8l.3.4a1.75 1.75 0 0 1-1.4 2.8z"/><circle cx="13.5" cy="6.5" r=".5" fill="currentColor"/><circle cx="17.5" cy="10.5" r=".5" fill="currentColor"/><circle cx="6.5" cy="12.5" r=".5" fill="currentColor"/><circle cx="8.5" cy="7.5" r=".5" fill="currentColor"/></svg>
         <svg
             width="12px"
             height="12px"
