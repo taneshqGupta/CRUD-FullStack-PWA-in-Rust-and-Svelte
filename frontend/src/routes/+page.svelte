1d319413a4ff8ee77b76546cdbb50b10717b46d9
taneshqGupta <f.taneshq@gmail.com>
2025-06-12 23:45:34 +0530

    refactored

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -87,7 +87,7 @@
 <div class="w-full max-w-md p-4 sm:p-0">
 
 	<div class="p-4">
-		<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 mx-auto">
+		<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
 			<legend class="fieldset-legend">New Task</legend>
 			<form on:submit|preventDefault={handleCreateTodo}>
 				<input
