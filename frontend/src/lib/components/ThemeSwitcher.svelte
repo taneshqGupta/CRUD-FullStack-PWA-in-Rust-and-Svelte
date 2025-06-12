c90927b57503c5be555d5e4c277cf374db83b309
taneshqGupta <f.taneshq@gmail.com>
2025-06-13 01:03:07 +0530

    refactored

--- frontend/src/lib/components/ThemeSwitcher.svelte
+++ frontend/src/lib/components/ThemeSwitcher.svelte
@@ -31,7 +31,12 @@
         role="button"
         class={`btn btn-ghost text-lg ${dropdownOpen ? 'bg-base-200' : ''}`}
     >
-        Themes
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-palette">
+            <rect x="5" y="3" width="14" height="18" rx="2"></rect>
+            <path d="M12 21V12"></path>
+            <path d="M5 9h14"></path>
+            <path d="M5 15h14"></path>
+        </svg>
         <svg
             width="12px"
             height="12px"
