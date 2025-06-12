049f789d8598ac114ae892185c3c53a84bd24667
taneshqGupta <f.taneshq@gmail.com>
2025-06-13 01:17:18 +0530

    refactored

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -129,7 +129,10 @@
         {/each}
 
         {#if !todos || todos.length === 0}
-            <li class="list-row p-4 text-center text-sm opacity-60">No tasks added yet.</li>
+            <li class="list-row p-4 text-center text-sm opacity-60">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-slash-icon lucide-circle-slash"><circle cx="12" cy="12" r="10"/><line x1="9" x2="15" y1="15" y2="9"/></svg>    
+                No tasks added yet.
+            </li>
         {/if}
         
     </ul>
