d9243b0f204195ff6944dc0364cb7f97a74b4d26
taneshqGupta <f.taneshq@gmail.com>
2025-06-14 18:25:43 +0530

    refactored

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,11 +1,6 @@
 // https://svelte.dev/docs/kit/types#app.d.ts
 declare global {
 	namespace App {
-		export interface Todo {
-			id: number;
-			descript: string;
-			done: boolean;
-		}
 		
 	}
 }
