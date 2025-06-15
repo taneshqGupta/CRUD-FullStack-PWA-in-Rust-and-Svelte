70863e6d84138b8aa8d290f44d81879cc9ab6abb
taneshqGupta <f.taneshq@gmail.com>
2025-06-15 12:49:50 +0530

    refactored

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -36,7 +36,7 @@
     <footer class="footer sm:footer-horizontal bg-neutral text-neutral-content items-center p-4">
     <aside class="grid-flow-col items-center">
         <SquirrelSvg />
-        <a href="/about" class="link link-hover">About This Project</a>
+        <a href="/about" class="link link-hover">About This Template</a>
     </aside>
     <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <a
