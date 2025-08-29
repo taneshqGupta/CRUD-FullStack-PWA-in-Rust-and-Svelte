c85c045f697e8e6e3807ac469b7c44ebdb61ca74
taneshqGupta <f.taneshq@gmail.com>
2025-08-30 04:06:46 +0530

    add map

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -146,7 +146,7 @@
 </svelte:head>
 
 <div class="bg-base-200 flex items-center justify-center">
-	<div class="w-full max-w-6xl max-h-6xl px-4 flex justify-center">
+	<div class="w-full h-full max-w-6xl px-4 flex justify-center">
 		<div class="card w-full max-w-md shadow-2xl bg-base-100">
 			<div class="card-body">
 				<div class="text-center mb-6">
