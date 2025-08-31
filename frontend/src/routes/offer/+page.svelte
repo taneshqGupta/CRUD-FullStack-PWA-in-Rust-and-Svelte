d54dcb2af912e6bf91afb1bdccba185991efcd52
taneshqGupta <f.taneshq@gmail.com>
2025-08-31 20:59:39 +0530

    hohoho

--- frontend/src/routes/offer/+page.svelte
+++ frontend/src/routes/offer/+page.svelte
@@ -140,7 +140,7 @@
 							>
 							<textarea
 								class="input textarea h-24 w-full"
-								placeholder="I can cook rice, design websites .."
+								placeholder="I can teach maths, design websites .."
 								bind:value={newPostDescription}
 								required
 							></textarea>
