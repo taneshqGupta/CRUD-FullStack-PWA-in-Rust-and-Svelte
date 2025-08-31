c68d33e8ae3e9cd84ae6532f9719f4dbcb14d37c
taneshqGupta <f.taneshq@gmail.com>
2025-08-31 17:51:06 +0530

    hohoho

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -90,4 +90,8 @@ export default defineConfig({
             },
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: process.env.PORT ? parseInt(process.env.PORT) : 3000
+    }
 });
