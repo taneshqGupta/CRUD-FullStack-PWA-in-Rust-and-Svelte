67e1d28bc0acca6adfd6156a0ddad4e53c1acdaf
taneshqGupta <f.taneshq@gmail.com>
2025-09-04 15:53:30 +0530

    hohoho

--- frontend/src/lib/components/InstallAppButton.svelte
+++ frontend/src/lib/components/InstallAppButton.svelte
@@ -49,7 +49,7 @@
 	<!-- lol -->
 	<button
 		id="installButton"
-		class="not-lg:hidden btn btn-soft btn-sm ml-2 text-xs"
+		class="not-sm:hidden btn btn-soft btn-sm ml-2 text-xs"
 		on:click={handleInstallClick}
 		aria-label="Install the application"
 	>
@@ -58,7 +58,7 @@
 	</button>
 	<button
 		id="installButton"
-		class="lg:hidden btn btn-soft btn-sm ml-2 text-xs"
+		class="sm:hidden btn btn-soft btn-sm ml-2 text-xs"
 		on:click={handleInstallClick}
 		aria-label="Install the application"
 	>
